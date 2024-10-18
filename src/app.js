d2b08f9c170081f24b0c6e2332115bb82eec285b
LukeIKG <luke.schmidt@outlook.de>
2024-10-18 13:00:59 +0200

    app.js

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const heading = document.createElement('h2');
+
+heading.textContent = 'Ich habs geschafft von app.js erstellt';
+
+document.body.appendChild(heading);
+
+console.log('Hallo von app.js, h2 erfolgreich hinzugefügt.');
