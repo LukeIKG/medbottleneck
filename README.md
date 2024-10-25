c01d5acd0dfc044025d5ec1d9f4c6bba03610727
LukeIKG <luke.schmidt@outlook.de>
2024-10-25 13:03:43 +0200

    Datenquelle im readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # medbottleneck
+
+# info:
+- div ids verwenden
+
+# datenquelle:
+- übersicht: https://anwendungen.pharmnet-bund.de/lieferengpassmeldungen/faces/public/meldungen.xhtml
+- RSS-Feed: https://anwendungen.pharmnet-bund.de/lieferengpassmeldungen/rss
--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-const heading = document.createElement('h2');
+const h2 = document.createElement('h2');
 
-heading.textContent = 'Ich habs geschafft von app.js erstellt';
+h2.textContent = 'Ich habs geschafft von app.js erstellt';
 
-document.body.appendChild(heading);
+document.body.appendChild(h2);
 
 console.log('Hallo von app.js, h2 erfolgreich hinzugefügt.');
+
+const h1 = document.querySelector('h1');
+h1.style.color = 'red';
+
+
+console.log('h1 rot .');
